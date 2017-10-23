62daf4b505643a23bc61226a8e4c5a8a703ee772
bininc <bininc@163.com>
2017-10-23 18:51:42 +0800

    first commit

---
+++ ClearInstalledSilverlight/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace ClearInstalledSilverlight
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblOS = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(16, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(344, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "本工具主要解决Silverligt程序无法安装等问题";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 234);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(341, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "程序会清理所有已安装的Silverlights程序，清理后请重新安装";
+            // 
+            // lblOS
+            // 
+            this.lblOS.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblOS.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblOS.Location = new System.Drawing.Point(30, 77);
+            this.lblOS.Name = "lblOS";
+            this.lblOS.Size = new System.Drawing.Size(312, 36);
+            this.lblOS.TabIndex = 2;
+            this.lblOS.Text = "Windows XP";
+            this.lblOS.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(109, 150);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(158, 35);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "清 理";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 255);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblOS);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Silverlight程序清理工具";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblOS;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ ClearInstalledSilverlight/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace ClearInstalledSilverlight
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnClear.Click += btnClear_Click;
+        }
+
+        void btnClear_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(lblOS.Text))
+            {
+                MessageBox.Show("未知操作系统，暂不支持！");
+                return;
+            }
+            OperatingSystem os = Environment.OSVersion;
+            string username = Environment.UserName;
+
+            string path = string.Format("C:\\Documents and Settings\\{0}\\Local Settings\\Application Data\\Microsoft\\Silverlight\\OutOfBrowser\\", username);
+            if (!Directory.Exists(path))
+            {
+                MessageBox.Show("未检测到可清理程序！");
+                return;
+            }
+
+            string[] regs = Directory.GetDirectories(path);
+            RegistryKey uninstallKey = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Uninstall", true);
+            foreach (string reg in regs)
+            {
+                string tmp = Path.GetFileName(reg);
+                if (tmp == "index") continue;
+                uninstallKey.DeleteSubKey(tmp, false);
+            }
+            uninstallKey.Close();
+
+            List<string> paths = new List<string>();
+            List<string> files = new List<string>();
+            paths.Add(path);
+
+            if (lblOS.Text != "Windows XP")
+            {
+                if (os.Platform == PlatformID.Win32NT && os.Version.Major >= 6)
+                {
+                    path = string.Format(@"C:\Users\{0}\AppData\LocalLow\Microsoft\Silverlight\OutOfBrowser\", username);
+                    if (Directory.Exists(path))
+                    {
+                        paths.Add(path);
+                    }
+                }
+            }
+
+
+            try
+            {
+                foreach (string p in paths)
+                {
+                    string[] fs = Directory.GetDirectories(p);
+                    if (fs.Length > 0)
+                    {
+                        files.AddRange(fs);
+                    }
+                }
+
+                if (files.Count > 0)
+                {
+                    foreach (string file in files)
+                    {
+                        Directory.Delete(file, true);
+                    }
+                }
+
+                string deskTop = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                string programs = Environment.GetFolderPath(Environment.SpecialFolder.Programs);
+
+                List<string> shortCuts = new List<string>();
+                string[] shortcutPaths = Directory.GetFiles(deskTop, "监控端*.lnk", SearchOption.TopDirectoryOnly);
+                shortCuts.AddRange(shortcutPaths);
+                shortcutPaths = Directory.GetFiles(programs, "监控端*.lnk", SearchOption.TopDirectoryOnly);
+                shortCuts.AddRange(shortcutPaths);
+                shortcutPaths = Directory.GetFiles(deskTop, "接警平台*.lnk", SearchOption.TopDirectoryOnly);
+                shortCuts.AddRange(shortcutPaths);
+                shortcutPaths = Directory.GetFiles(programs, "接警平台*.lnk", SearchOption.TopDirectoryOnly);
+                shortCuts.AddRange(shortcutPaths);
+                shortcutPaths = Directory.GetFiles(deskTop, "110接警*.lnk", SearchOption.TopDirectoryOnly);
+                shortCuts.AddRange(shortcutPaths);
+                shortcutPaths = Directory.GetFiles(programs, "110接警*.lnk", SearchOption.TopDirectoryOnly);
+                shortCuts.AddRange(shortcutPaths);
+
+                foreach (string shortCut in shortCuts)
+                {
+                    File.Delete(shortCut);
+                }
+
+
+                MessageBox.Show("清理成功！");
+            }
+            catch
+            {
+                MessageBox.Show("清理失败，请重试！");
+            }
+        }
+
+
+        protected override void OnLoad(EventArgs e)
+        {
+            lblOS.Text = GetOSName();
+            base.OnLoad(e);
+        }
+
+        public string GetOSName()
+        {
+            OperatingSystem os = Environment.OSVersion;
+            string osname = "";
+            switch (os.Platform)
+            {
+                case PlatformID.Win32Windows:
+                    switch (os.Version.Minor)
+                    {
+                        case 0:
+                            osname = "Windows 95";
+                            break;
+                        case 10:
+                            if (os.Version.Revision.ToString() == "2222A")
+                                osname = "Windows 98 第二版";
+                            else
+                                osname = "Windows 98";
+                            break;
+                        case 90:
+                            osname = "Windows Me";
+                            break;
+                    }
+                    break;
+                case PlatformID.Win32NT:
+                    switch (os.Version.Major)
+                    {
+                        case 3:
+                            osname = "Windows NT 3.51";
+                            break;
+                        case 4:
+                            osname = "Windows NT 4.0";
+                            break;
+                        case 5:
+                            switch (os.Version.Minor)
+                            {
+                                case 0:
+                                    osname = "Windows 200";
+                                    break;
+                                case 1:
+                                    osname = "Windows XP";
+                                    break;
+                                case 2:
+                                    osname = "Windows 2003";
+                                    break;
+                            }
+                            break;
+                        case 6:
+                            switch (os.Version.Minor)
+                            {
+                                case 0:
+                                    osname = "Windows Vista";
+                                    break;
+                                case 1:
+                                    osname = "Windows 7/2008";
+                                    break;
+                                case 2:
+                                    osname = "Windows 8/10";
+                                    break;
+                            }
+                            break;
+                        case 10:
+                            switch (os.Version.Minor)
+                            {
+                                case 0:
+                                    osname = "Windows 10";
+                                    break;
+                            }
+                            break;
+                    }
+                    break;
+            }
+            return osname;
+        }
+    }
+}
